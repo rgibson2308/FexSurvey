621345bb6273f714f476999f7a3afd26e25af286
rgibson2308 <rgibson2308@gmail.com>
2025-11-20 19:02:02 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,24 @@
 // pages/index.tsx
 import { builder, BuilderComponent } from '@builder.io/react';
-import type { GetStaticProps } from 'next';
 
-// Your Builder.io Space ID
-builder.init('8a6b0454e24748259b112cb302d9c5f8');
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-type Props = {
-  content: any;
-};
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const content = await builder
-    .get('page', {
-      userAttributes: { urlPath: '/' },
-    })
+export async function getStaticProps({ params }) {
+  const urlPath = '/' + (params?.page?.join('/') || '');
+  const page = await builder
+    .get('page', { userAttributes: { urlPath } })
     .toPromise();
 
   return {
-    props: {
-      content: content || null,
-    },
-    // Regenerate page every 5 seconds if updated in Builder
+    props: { page: page || null },
     revalidate: 5,
   };
-};
+}
 
-export default function HomePage({ content }: Props) {
-  return (
-    <div>
-      <BuilderComponent model="page" content={content} />
-    </div>
-  );
+export async function getStaticPaths() {
+  return { paths: [], fallback: 'blocking' };
+}
+
+export default function BuilderPage({ page }) {
+  return <BuilderComponent model="page" content={page} />;
 }
